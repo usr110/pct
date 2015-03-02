3afb05da17fa8f93f9082619acc6680b7536f50a
usr110 <aa797@cdpc42.medschl.cam.ac.uk>
2015-03-02 13:51:14 +0000

    -- Added MSOA data -- Added scenarios for CLC, PLC and ECP lines -- Added top and bottom lines

--- shiny-test/spatial-lines-test/server.R
+++ shiny-test/spatial-lines-test/server.R
@@ -1,17 +1,60 @@
 library(shiny)
 library(leaflet)
-library(ggmap)
-library(rgdal)
+library(sp)
 
-# Load data
-map_centre <- geocode("Leeds")
+# Load line data
 l <- readRDS("l.Rds")
+# Load MSOA data
+leeds <- readRDS("leeds-msoas-simple.Rds")
 
-function(input, output){
-  map = leaflet(data = l) %>%
-    addTiles() %>%
-    setView(lng = map_centre[1], lat = map_centre[2], zoom = 10) %>%
-    addPolylines()
-  output$map = renderLeaflet(map)
+function(input, output, session) {
 
+  observe({
+    # Select which lines are to be shown - when user selects the scenario
+    choice <- ""
+    if (input$scenario == 1){
+      choice <- "clc"
+    }else if (input$scenario == 2){
+      choice <- "plc"
+    }else if (input$scenario == 3){
+      choice <- "ecp"
+    }
+
+    ## Tail of Head of sorting (if it is TOP then it is sorted by head, otherwise by tail)
+    torh <- 0
+    func <- head
+
+    if (input$lines == "Top 10"){
+      torh <- 10
+      func <- head
+    }else if (input$lines == "Top 50"){
+      torh <- 50
+      func <- head
+    }else if (input$lines == "Bottom 10"){
+      torh <- 10
+      func <- tail
+    }else if (input$lines == "Bottom 50"){
+      torh <- 50
+      func <- tail
+    }
+
+    # Calculate the filtered data based on order function (head or tail) and how many lines are shown
+    filter_data <- l[ func(order(l[[choice]]), (torh)), ]
+
+    map <- leaflet() %>%
+      addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>%
+      addPolylines(data = filter_data, opacity = 1, color = "red") %>%
+      mapOptions(zoomToLimits = "first")
+    ## Add polygones (of MSOA boundaries) ONLY when the checkbox transp_zones is CHECKED
+    if (input$show_zones == TRUE){
+      map <- map %>%
+        addPolygons(data = leeds
+                    , fillOpacity = 0.4
+                    , opacity = (input$transp)*.4
+                    , fillColor = leeds$color_pcycle
+        )
+    }
+    output$map = renderLeaflet(map)
+  })
 }
+
